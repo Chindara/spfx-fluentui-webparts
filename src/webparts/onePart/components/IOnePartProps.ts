f0dfe2a1fbe49290c6d1c43ca64da36c36fc6176
Chinthaka Bandara <chinthakapb@gmail.com>
2022-11-25 12:14:38 +0530

    [feat] OnePart added.

---
+++ src/webparts/onePart/components/IOnePartProps.ts
@@ -0,0 +1,7 @@
+export interface IOnePartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/onePart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IOnePartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'OnePartWebPartStrings' {
+  const strings: IOnePartWebPartStrings;
+  export = strings;
+}
