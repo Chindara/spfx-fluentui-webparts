856a2b2685bd1841b25d536d5d0fe44c40d574c8
Chinthaka Bandara <chinthakapb@gmail.com>
2023-03-21 16:26:08 +0530

    [chore] editableGrid WebPart added

---
+++ src/webparts/editableGrid/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IEditableGridWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'EditableGridWebPartStrings' {
+  const strings: IEditableGridWebPartStrings;
+  export = strings;
+}
